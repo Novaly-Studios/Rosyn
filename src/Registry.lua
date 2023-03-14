54a19d36de3b00eb032d337a75d6545f83bb1c9f
tpc9000 <adam.mabin@googlemail.com>
2023-03-14 23:24:39 +0000

    Clean up Registry type defs

--- src/Registry.lua
+++ src/Registry.lua
@@ -4,78 +4,79 @@ local Rosyn = require(script.Parent)
 type ValidComponentClassOrInstance = Rosyn.ValidComponentClassOrInstance
 type XSignal<T...> = XSignal.XSignal<T...>
 
-type RosynRegistry<T> = {
-    GetInstances: ((RosynRegistry<T>) -> ({[Instance]: true}));
-    GetComponent: ((RosynRegistry<T>) -> (T));
-    GetComponents: ((RosynRegistry<T>) -> ({[T]: true}));
-    ExpectComponent: ((RosynRegistry<T>) -> (T));
-    AwaitComponentInit: ((RosynRegistry<T>) -> (T));
-    GetComponentFromDescendant: ((RosynRegistry<T>) -> (T));
+type Registry<T> = {
+    GetInstances: ((Registry<T>) -> ({[Instance]: true}));
+    GetComponent: ((Registry<T>, Instance) -> (T?));
+    GetComponents: ((Registry<T>) -> ({[T]: true}));
+    ExpectComponent: ((Registry<T>, Instance) -> (T));
+    AwaitComponentInit: ((Registry<T>, Instance) -> (T));
+    GetComponentFromDescendant: ((Registry<T>, Instance) -> (T?));
 
     Events: {
         OnComponentAdded: XSignal<T>;
         OnComponentRemoving: XSignal<T>;
-        OnComponentInitialized: XSignal<T>;
     };
 };
 
-local _RegistryCache: {[ValidComponentClassOrInstance]: RosynRegistry<any>} = {}
+local _RegistryCache: {[ValidComponentClassOrInstance]: Registry<any>} = {}
 
-local RosynRegistry = {}
-RosynRegistry.__index = RosynRegistry
+local Registry = {}
+Registry.__index = Registry
 
-function RosynRegistry.new(ComponentClass)
-    return setmetatable({
+function Registry.new<T>(ComponentClass: T): Registry<T>
+    local self = setmetatable({
         _ComponentClass = ComponentClass;
 
         Events = {
             OnComponentAdded = Rosyn.GetAddedSignal(ComponentClass);
             OnComponentRemoving = Rosyn.GetRemovingSignal(ComponentClass);
         };
-    }, RosynRegistry)
+    }, Registry)
+
+    return self
 end
 
 --- Obtains all components corresponding to the registry's type.
-function RosynRegistry:GetComponents()
+function Registry:GetComponents()
     return Rosyn.GetComponentsOfClass(self._ComponentClass)
 end
 
 --- Obtains all Instances corresponding to the registry's type.
-function RosynRegistry:GetInstances()
+function Registry:GetInstances()
     return Rosyn.GetInstancesOfClass(self._ComponentClass)
 end
 
 --- Obtains a component corresponding to the registry's type, from the given Instance.
-function RosynRegistry:GetComponent(FromInstance)
+function Registry:GetComponent(FromInstance)
     return Rosyn.GetComponent(FromInstance, self._ComponentClass)
 end
 
 --- Waits for a component to be initialized on the given Instance.
-function RosynRegistry:AwaitComponentInit(FromInstance)
+function Registry:AwaitComponentInit(FromInstance)
     return Rosyn.AwaitComponentInit(FromInstance, self._ComponentClass)
 end
 
 --- Obtains a component corresponding to the registry's type, from the given Instance.
-function RosynRegistry:ExpectComponent(FromInstance)
+function Registry:ExpectComponent(FromInstance)
     return Rosyn.ExpectComponent(FromInstance, self._ComponentClass)
 end
 
 --- Obtains a component corresponding to the registry's type, from a given Instance or any of its ancestors.
-function RosynRegistry:GetComponentFromDescendant(FromInstance)
+function Registry:GetComponentFromDescendant(FromInstance)
     return Rosyn.GetComponentFromDescendant(FromInstance, self._ComponentClass)
 end
 
 --- Finds or creates a registry for the given component type.
-function RosynRegistry.GetRegistry<T>(ComponentClass: T & ValidComponentClassOrInstance): RosynRegistry<T>
+function Registry.GetRegistry<T>(ComponentClass: T & ValidComponentClassOrInstance): Registry<T>
     local Target = _RegistryCache[ComponentClass]
 
     if (Target) then
         return Target
     end
 
-    Target = RosynRegistry.new(ComponentClass)
+    Target = Registry.new(ComponentClass)
     _RegistryCache[ComponentClass] = Target
     return Target
 end
 
-return RosynRegistry
+return Registry
